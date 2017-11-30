2edbb56a2fd9f5d224f3df8b4f61a3f4f1fc3082
Gavin McNamara <gavin.mcnamara01@myhunter.cuny.edu>
2017-11-30 15:15:54 -0500

    Created function append that adds an element to end of linked list

--- question5.py
+++ question5.py
@@ -7,7 +7,26 @@
 # class below to use as a representation of a node in the
 # linked list. Return the value of the node at that position.
 
-class Node(object):
-  def __init__(self, data):
-    self.data = data
-    self.next = None
+# An Element has some value associated with it (which could
+# be anything—a number, a string, a character, et cetera),
+# and it has a variable that points to the next element
+# in the linked list.
+class Element(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def append(self, new_element):
+        current = self.head
+        # If the LinkedList already has a head, iterate through
+        # the next reference in every Element until you reach
+        # the end of the list.
+        if self.head:
+            while current.next:
+                current = current.next
+            #Set next for the end of the list to be the new_element
+            current.next = new_element
+        else:
+            # if there is no head already, you should just assign
+            # new_element to it and do nothing else.
+            self.head = new_element
