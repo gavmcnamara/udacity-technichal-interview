fdfb81d897138cc166e11043fbd638021d5d9d62
Gavin McNamara <gavin.mcnamara01@myhunter.cuny.edu>
2017-12-01 16:28:32 -0500

    feat: Created push and question5 functions
    The push function adds a new node at the beginning of our linked list. The question5 function goes through our linked list and finds the node the user is looking for

--- question5.py
+++ question5.py
@@ -7,8 +7,7 @@
 # class below to use as a representation of a node in the
 # linked list. Return the value of the node at that position.
 
-# An Element has some value associated with it (which could
-# be anything—a number, a string, a character, et cetera),
+# An Element has some value associated with it
 # and it has a variable that points to the next element
 # in the linked list.
 class Element(object):
@@ -16,17 +15,52 @@ class Element(object):
         self.value = value
         self.next = None
 
-    def append(self, new_element):
-        current = self.head
-        # If the LinkedList already has a head, iterate through
-        # the next reference in every Element until you reach
-        # the end of the list.
-        if self.head:
-            while current.next:
-                current = current.next
-            #Set next for the end of the list to be the new_element
-            current.next = new_element
-        else:
-            # if there is no head already, you should just assign
-            # new_element to it and do nothing else.
-            self.head = new_element
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+
+# This function is in LinkedList class. It inserts
+    # a new node at the beginning of Linked List.
+    def push(self, new_data):
+
+        # 1 & 2: Allocate the Element &
+        #     Put in the data
+        new_node = Element(new_data)
+
+        # 3. Make next of new Node as head
+        new_node.next = self.head
+
+        # 4. Move the head to point to new Node
+        self.head = new_node
+
+    def question5(ll, m):
+        current = ll.head
+        count = 0
+
+        while (current):
+            if (count == m):
+                return current.value
+            count += 1
+            current = current.next
+        assert(false)
+        return 0;
+
+
+# Code execution starts here
+if __name__=='__main__':
+
+    llist = LinkedList()
+
+    # Use push() to construct below list
+    # 1->12->1->4->1
+    llist.push(1);
+    llist.push(4);
+    llist.push(1);
+    llist.push(12);
+    llist.push(1);
+
+    n = 3
+    print ("Element at index 3 is :", llist.question5(n))
+
+
+# Time Complexity: O(n)
