a88c3e8e7854b3df955fa99902b000f944bed5b3
Gavin McNamara <gavin.mcnamara01@myhunter.cuny.edu>
2017-12-01 17:40:53 -0500

    tests: added tests to meet requirements

--- question5.py
+++ question5.py
@@ -21,19 +21,13 @@ class LinkedList(object):
 
 # This function is in LinkedList class. It inserts
     # a new node at the beginning of Linked List.
-    def push(self, new_data):
-
-        # 1 & 2: Allocate the Element &
-        #     Put in the data
-        new_node = Element(new_data)
-
-        # 3. Make next of new Node as head
+    def push(self, new_value):
+        new_node = Element(new_value)
         new_node.next = self.head
-
-        # 4. Move the head to point to new Node
         self.head = new_node
 
     def question5(ll, m):
+
         current = ll.head
         count = 0
 
@@ -42,25 +36,84 @@ class LinkedList(object):
                 return current.value
             count += 1
             current = current.next
-        assert(false)
+        return None
         return 0;
 
+# Since the push function adds a node to the
+# beginning of the ll, then thechnically the first node
+# that is inserted will be the last in the length of ll
 
-# Code execution starts here
+# Execution of the code
+# Should return 67 as its value
 if __name__=='__main__':
 
-    llist = LinkedList()
+    ll = LinkedList()
 
-    # Use push() to construct below list
-    # 1->12->1->4->1
-    llist.push(1);
-    llist.push(4);
-    llist.push(1);
-    llist.push(12);
-    llist.push(1);
+    # Push function to add nodes
+    # 5->67->3->123->6
+    ll.push(5);
+    ll.push(67);
+    ll.push(3);
+    ll.push(123);
+    ll.push(6);
 
-    n = 3
-    print ("Element at index 3 is :", llist.question5(n))
+    m = 3
+    print ("Element at index 2 is :", ll.question5(m))
 
+# Should return 21323353 as its value
+if __name__=='__main__':
 
-# Time Complexity: O(n)
+    ll = LinkedList()
+
+    # Push function to add nodes
+    # 5->67->3->123->6
+    ll.push(21323353);
+    ll.push(21356456);
+    ll.push(06453436);
+    ll.push(12314325);
+    ll.push(63253534);
+
+    m = 4
+    print ("Element at index 4 is :", ll.question5(m))
+
+# This will check for a value below the length
+# of ll and should return None as its value
+if __name__=='__main__':
+
+    ll = LinkedList()
+
+    # Push function to add nodes
+    # 5->67->3->123->6
+    ll.push(0);
+    ll.push(14);
+    ll.push(345);
+    ll.push(12546);
+    ll.push(6325353);
+
+    m = -1
+    print ("Element at index -1 is :", ll.question5(m))
+
+# This will check for a value above the length
+# of ll and should return None as its value
+if __name__=='__main__':
+
+    ll = LinkedList()
+
+    # Push function to add nodes
+    # 5->67->3->123->6
+    ll.push(0);
+    ll.push(14);
+    ll.push(345);
+    ll.push(12546);
+    ll.push(6325353);
+
+    m = 10
+    print ("Element at index 10 is :", ll.question5(m))
+# Time Complexity: O(n) because it visits all the nodes
+# then finds the node at position m
+
+# Code Design: This algorithm initializes count = 0 and then
+# loops through the link listand if count is equal to
+# m then it will return the current node and increment
+# the count. Then will change the current point
+# to the next current.
